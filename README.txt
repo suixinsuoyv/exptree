8ffb7e3c22f45e00bf6965dcfae2a54eadff6555
tsadowski <tsadowski93@gmail.com>
2020-02-05 17:21:27 -0500

    First commit, functional program

---
+++ README.txt
@@ -0,0 +1,18 @@
+TOM'S EXPRESSION TREE CONSTRUCTOR AND EVALUATOR
+
+For this program to run, you must first install python 3.6 which can be found at 
+python.org/downloads/release/python-360/
+
+To run this program:
+1) open your terminal to the directory where exptree.py was installed.
+2) enter the following command: 
+	'python3 exptree.py'
+3) follow the prompt, it should say 'enter an infix expression delimited by spaces (integers only):'.
+
+An infix expression delimited by spaces is an arithmetic expression where each operator 
+or operand is seperated by spaces. For example:
+	5 + 3 - 1
+is valid (and it evaluates to 7), however
+	5+3-1
+is invalid. If you have entered something incorrectly, the program will terminal with an error message. 
+
--- exptree.py
+++ exptree.py
@@ -1,16 +1,17 @@
-
+# Thomas Sadowski
+# checks if a string is a digit
 def isdi(c):
 	for i in c:
 		if not (i >= '0' and i <= '9'):
 			return False
 	return True
-
+# checks if string is an operator
 def isop(c):
 	ops = {'+', '-', '*', '/', '^'}
 	if len(c) > 1 or c[0] not in ops:
 		return False
 	return True;
-
+# returns precedence of operator
 def prec(c):
 	if c == '-' or c == '+':
 		return 1
@@ -20,7 +21,7 @@ def prec(c):
 		return 3
 	else:
 		return 0
-	
+# converts infix expression to postfix expression
 def intopost(exp):
 	stack = list()
 	post = list()
@@ -37,14 +38,17 @@ def intopost(exp):
 	while not (len(stack)-1) == 0:
 		post.append(stack.pop())
 	return post
-
+# conventional node for a binary tree, with one extra variable
 class Node:
-	__slots__ = ['left', 'right', 'data']
+	__slots__ = ['left', 'right', 'data', 'alti']
 	def __init__(self, data):
 		self.left = None
 		self.right = None
 		self.data = data
-
+		# alti = altitude--however, it is only useful for denoting the height of the tree 
+		# which is found at the root node
+		self.alti = 0 
+# builds the expression tree out of nodes of class Node
 def exptree(exp):
 	stack = list()
 	for i in exp:
@@ -52,42 +56,86 @@ def exptree(exp):
 		if isdi(i):
 			stack.append(n)
 		else:
-			n.right = stack.pop()
-			n.left = stack.pop()
+			r = stack.pop()
+			n.right = r
+			l = stack.pop()
+			n.left = l
+			n.alti = max(r.alti, l.alti) + 1
 			stack.append(n)
 	return stack.pop()	
-
+# performs a level-order traversal on the tree
+def levelorder(tree):
+	for i in range(0, tree.alti + 1):
+		printlevel(tree, i)
+		print()
+# helper for levelorder
+def printlevel(tree, level):
+	if tree is None:
+		return
+	if level == 0:
+		print(tree.data, end=" ")
+	else:
+		printlevel(tree.left, level - 1)
+		printlevel(tree.right, level -1)
+# performs an in-order traversal on the tree
 def inorder(tree):
 	if tree is not None:
 		inorder(tree.left)
 		print(tree.data, end=" ")
 		inorder(tree.right)
-
+# performs a pre-order traversal on the tree
 def preorder(tree):
 	if tree is not None:
 		print(tree.data, end=" ")
 		preorder(tree.left)
 		preorder(tree.right)
-
+# performs a post-order traversal on the tree
 def postorder(tree):
 	if tree is not None:
 		postorder(tree.left)
 		postorder(tree.right)
 		print(tree.data, end=" ")
 
+def evaluate(exp):
+	stack = list()
+	for i in exp:
+		if isdi(i):
+			i = int(i)
+			stack.append(i)
+		else:
+			b = stack.pop()
+			a = stack.pop()
+			stack.append(operate(a, b, i))
+	return stack.pop()
+
+def operate(a, b, op):
+	if op == '^':
+		return a**b 
+	if op == '*':
+		return a*b
+	if op == '/':
+		return a//b
+	if op == '+':
+		return a+b
+	else:
+		return a-b
+# main 
 if __name__ == "__main__":
-	print("enter the expression in infix notation delimited by spaces. ")
+	print("Enter an infix expression delimited by spaces (integers only). ")
 	exp = input()
 	exp = exp.split()
 	exp = intopost(exp)
-	
 	n = exptree(exp)	
-	inorder(n)
-	print()
+	
+	print("Expression tree: ")
+	levelorder(n)
+	print("Pre-order traversal / prefix / polish notation: ")
 	preorder(n)
-	print()
+	print("\nPost-order traversal / postfix / reverse polish notation: ")
 	postorder(n)
-	print()
-	print(exp)
+	print("\nIn-order traversal / infix notation: ")
+	inorder(n)
+	print("\nEvaluation of expression: ")
+	print(evaluate(exp))
 	
 
